e75f18a5205772462dbb8a5d40f62d1df8901de0
essazafar <ezafar544@gmail.com>
2026-08-29 06:56:45 -0700

    Create 26P3086_task3.c

---
+++ 26P3086_task3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	printf("================= C ESCAPE SEQUENCES REFRENCES=====================================\n");
+	printf("1. To print a double quote, write :\t\t\\\"\n");
+	printf("2. To print a literal backslash, write:\t\t\\\\\n");
+	printf("3. To print a new line break, write : \t\t\\n\n");
+    printf("4. To print a horizontle tab stop, write :\t\\t\n");
+    printf("5. To print a percent sign , write:\t\t%%%%\n");
+    printf("===================================================================================\n");
+	return 0;
+}
