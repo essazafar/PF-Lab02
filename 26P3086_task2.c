823545ac8243745ea5a76c585da7aa34944e207d
essazafar <ezafar544@gmail.com>
2026-08-29 06:01:43 -0700

    Create 26P3086_task2.c

---
+++ 26P3086_task2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+	int batchYear = 2026;
+	char section = 'B';
+	float testScore = 82.75;
+	int semesterFee = 268560;
+
+	printf("=======================================================\n");
+	printf("\tFAST-NUCES STUDENT PROFILE CARD\n");
+	printf("=======================================================\n");
+	printf("Batch Year : \t%d\n",batchYear);
+	printf("section :\t%c\n",section);
+	printf("testScore :\t%.2f\n",testScore);
+	printf("semesterFee :\tPKR %d\n",semesterFee);
+	printf("Motto : \t\"Small daily steps lead to big success \"\n");
+	printf("=======================================================\n");
+
+	return 0;
+}
